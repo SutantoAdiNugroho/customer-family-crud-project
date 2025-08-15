68bc3e581b64267162660f1833dc8dc078ff18e0
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2025-08-15 10:10:00 +0700

    feat: initilaize go project, define model and driver database

---
+++ backend-go/domain/model/customer.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Customer struct {
+	CstID         int       `json:"cst_id"`
+	NationalityID int       `json:"nationalityI_id"`
+	CstName       string    `json:"cst_name"`
+	CstDob        time.Time `json:"cst_dob"`
+	CstPhoneNum   string    `json:"cst_phoneNum"`
+	CstEmail      string    `json:"cst_email"`
+}
